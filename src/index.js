8a77614d6f7f41f83dd624965534b0ffdaa59bbe
Nebojsa Nedeljkovic <nebojsa.nedeljkovic@nortal.com>
2020-01-16 16:33:12 +0100

    handling css with webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import helloWorld from './hello-world.js';
+import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
 import addImage from './add-image.js';
 
-helloWorld();
-addImage();
+const helloWorldButton = new HelloWorldButton();
+helloWorldButton.render();
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
       {
         test: /\.(png|jpg)$/,
         use: ['file-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   }
