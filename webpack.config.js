321d021035f1abfc92a3cd0307e62dcda92c0828
Nebojsa Nedeljkovic <nebojsa.nedeljkovic@nortal.com>
2020-01-22 16:52:59 +0100

    generate html files automatically

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, './dist'),
-    publicPath: 'dist/'
+    publicPath: ''
   },
   mode: 'none',
   module: {
@@ -43,11 +44,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'styles.[contenthash].css'
     }),
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: [
-        '**/*',
-        path.join(process.cwd(), 'build/**/*')
-      ]
-    })
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin()
   ]
 };
