b0cd53aa8e3f201b1d6b50c26c9d8f32af2aad02
Nebojsa Nedeljkovic <nebojsa.nedeljkovic@nortal.com>
2020-01-22 16:27:07 +0100

    extract css part 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
-import addImage from './add-image.js';
+import Heading from './components/heading/heading.js';
+
+
+const heading = new Heading();
+heading.render();
+
 
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
