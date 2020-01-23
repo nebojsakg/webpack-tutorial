a25aab6d98886b9f48d9fc1dd006ea086ba7ae67
Nebojsa Nedeljkovic <nebojsa.nedeljkovic@nortal.com>
2020-01-23 11:25:21 +0100

    custom options

--- src/kiwi.js
+++ src/kiwi.js
@@ -1,9 +1,9 @@
 import Heading from './components/heading/heading.js';
 import KiwiImage from './components/kiwi-image/kiwi-image.js';
-import _ from 'lodash';
+import React from 'react';
 
 const heading = new Heading();
-heading.render(_.upperFirst('kiwi'));
+heading.render('kiwi');
 
 const kiwiImage = new KiwiImage();
 kiwiImage.render();
