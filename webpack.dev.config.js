f161825fd21869b3719868766ee37310a5b247f7
Nebojsa Nedeljkovic <nebojsa.nedeljkovic@nortal.com>
2020-01-23 10:32:38 +0100

    dev/prod config

---
+++ webpack.dev.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: ''
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env'],
+                        plugins: ['transform-class-properties']
+                    }
+                }
+            },
+            {
+                test: /\.hbs$/,
+                use: [
+                    'handlebars-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Hello world',
+            // filename: 'subfolder/custom_filename.html',
+            template: 'src/index.hbs',
+            description: 'Some description'
+        })
+    ]
+};
